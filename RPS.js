7f2d390d7d0bf850ceb4019fda63eb85346e99c9
Eric Romano <ecr1980@gmail.com>
2022-01-15 23:57:16 -0500

    Added all the functions to make this work properly, and tested. Final result per the initial project. Script accurately keeps and displays five rounds, as well as keeps track of the score and declaring a winner. Script also displays current score during each round and the final score at the end. Admittedly, this should have been committed several times during the writing and testing of the script. That is part of the learning process, though, isn't it? And I'll be surprised if anyone reads this. But if you do, know that I have realized my mistake on my own and will attempt to form a better habit as far as saves, commits, and documentation go.

--- RPS.js
+++ RPS.js
@@ -27,6 +27,78 @@ function pickEquals(pick)
 function computerPlay()
 {
     let compPlay = pickEquals(computerSelection());
-    console.log(compPlay); 
+    return compPlay; 
 }
 
+// non strict comparisons do not negate capitalizations. In other words......
+//  rock == Rock will be false.
+
+//This is for the player to choose.
+
+function playerSelection(){
+    let playerChoice = prompt("Please choose. Rock, Paper, or Scissors.");
+    let playerChoice2 = playerChoice.toLowerCase();
+    playerChoice = playerChoice2[0].toUpperCase() + playerChoice2.substring(1);
+    return playerChoice;
+}
+
+function playRound()
+{
+    let playerPick;
+    let compPick;
+    let score = [0,0];
+
+    while (playerPick != 'Rock' && playerPick != 'Paper' && playerPick != "Scissors")
+    {
+        playerPick = playerSelection();
+    }
+
+    compPick = computerPlay();
+
+    if (playerPick == compPick)
+    {
+        console.log('You both picked ' + compPick + '.');
+        
+    }
+    else if((playerPick == 'Rock' && compPick == 'Paper') || (playerPick == 'Paper' && compPick == 'Scissors') || (playerPick == 'Scissors' && compPick == 'Rock'))
+    {
+        console.log('You lose. ' + compPick + ' beats ' + playerPick + '.');
+        score[1] = 1;
+    }
+    else{
+        console.log('You win! ' + playerPick + ' beats ' + compPick +'!');
+        score[0] = 1;
+    }
+
+    return score;
+}
+
+function game()
+{
+    let playerScore = 0;
+    let compScore = 0;
+    let combinedScore = [0,0];
+
+    for(let i = 0; i<5; i++)
+    {
+        console.log('Round ' + (+i+1) +'!');
+        console.log('Current score is Player ' + playerScore + ' and Computer ' + compScore +'.');
+        combinedScore = playRound();
+        playerScore += combinedScore[0];
+        compScore += combinedScore[1];
+    }
+
+    if(playerScore > compScore)
+    {
+        console.log('You Win!');
+    }
+    else if(compScore > playerScore)
+    {
+        console.log('Computer Wins!');
+    }
+    else{
+        console.log('Tie game.');
+    }
+
+    console.log('Final Score, Player ' + playerScore + '. Computer ' + compScore + '.');
+}
